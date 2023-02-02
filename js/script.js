acf15a589525bf14e9a3ef47f5817a35cee2168f
Daniel <118988486+danax7@users.noreply.github.com>
2023-02-02 16:24:02 +0700

    Добавлена форма в раздел "Оформить заказ"
    Пока без отправки данных на сервер. Для полей формы был подключен Bootstrap(для теста)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
-console.log("Hello world!")
+let buttonCountPlus = document.getElementById("buttonCountPlus");
+let buttonCountMinus = document.getElementById("buttonCountMinus");
+let count = document.getElementById("buttonCountNumber");
+let count2 = document.getElementById("num");
+let number = 1;
+let price = 350;
 
+buttonCountPlus.onclick = function() {
+    if (number <= 3) {
+        number++;
+        count.innerHTML = number;
+        count2.value = number * price;
+    }
+};
+
+buttonCountMinus.onclick = function() {
+    if (number >= 2) {
+        number--;
+        count.innerHTML = number;
+        count2.value = number * price;
+    }
+}
